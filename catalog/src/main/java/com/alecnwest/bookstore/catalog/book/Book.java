cb9a25dd6c8c28c48d7cc77b836fe22a7167e0ea
Alec West <alecwest38@gmail.com>
2023-08-26 10:22:06 -0600

    feat(catalog): simplify with JpaRepository

--- catalog/src/main/java/com/alecnwest/bookstore/catalog/book/Book.java
+++ catalog/src/main/java/com/alecnwest/bookstore/catalog/book/Book.java
@@ -3,17 +3,25 @@ package com.alecnwest.bookstore.catalog.book;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.rest.core.annotation.Description;
 
 import java.util.List;
 
+/**
+ * You can get types by adding Accept: application/schema+json to the header
+ * https://docs.spring.io/spring-data/rest/docs/current/reference/html/#metadata.alps
+ */
 @Entity
 @Data
 @RequiredArgsConstructor
-@NamedQuery(name = "Book.findByGenre", query = "from Book where ?1 MEMBER OF genre")
 public class Book {
 
     private @Id @GeneratedValue(strategy = GenerationType.UUID) String id;
+
+    @Description("The book's title")
     private final String title;
+
+    @Description("The book's author")
     private final String author;
 
     private String isbn10;
@@ -22,6 +30,7 @@ public class Book {
 
     // ElementCollection made MEMBER OF named query possible https://stackoverflow.com/questions/2687690/jpa-2-and-hibernate-3-5-1-member-of-query-doesnt-work
     @ElementCollection
+    @Description("The genres that describe the book")
     private List<String> genre;
 
     protected Book() {
--- catalog/src/main/java/com/alecnwest/bookstore/catalog/book/BookRepository.java
+++ catalog/src/main/java/com/alecnwest/bookstore/catalog/book/BookRepository.java
@@ -1,15 +1,22 @@
 package com.alecnwest.bookstore.catalog.book;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface BookRepository extends JpaRepository<Book, String> {
-     // table 2.3 at https://docs.spring.io/spring-data/jpa/docs/1.6.0.RELEASE/reference/html/jpa.repositories.html
-    List<Book> findByAuthorLike(String author);
+    // used % signs to get partial matches https://www.postgresql.org/docs/9/functions-matching.html
+    // table 2.3 at https://docs.spring.io/spring-data/jpa/docs/1.6.0.RELEASE/reference/html/jpa.repositories.html
+    @Query("select b from Book b where b.author like %?1%")
+    List<Book> findByAuthor(String author);
 
-     // table 2.3 at https://docs.spring.io/spring-data/jpa/docs/1.6.0.RELEASE/reference/html/jpa.repositories.html
-    List<Book> findByTitleLike(String title);
 
+    // used % signs to get partial matches https://www.postgresql.org/docs/9/functions-matching.html
+    // table 2.3 at https://docs.spring.io/spring-data/jpa/docs/1.6.0.RELEASE/reference/html/jpa.repositories.html
+    @Query("select b from Book b where b.title like %?1%")
+    List<Book> findByTitle(String title);
+
+    @Query("from Book where ?1 MEMBER OF genre")
     List<Book> findByGenre(String genre);
 }
