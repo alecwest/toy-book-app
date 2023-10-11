148f3de9ae233954acc6cc8b58177a57b6153c3d
Alec West <alecwest38@gmail.com>
2023-10-10 18:33:30 -0600

    feat(catalogFinder): initial commit

---
+++ catalogFinder/src/main/java/com/alecnwest/bookstore/catalogFinder/CatalogFinderApplication.java
@@ -0,0 +1,13 @@
+package com.alecnwest.bookstore.catalogFinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogFinderApplication.class, args);
+	}
+
+}
---
+++ catalogFinder/src/test/java/com/alecnwest/bookstore/catalogFinder/CatalogFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alecnwest.bookstore.catalogFinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
